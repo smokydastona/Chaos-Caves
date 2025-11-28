760b9cc11b4ad0b95f5149dc8eaac82f50e6bc15
smokydstona <smokydstona@gmail.com>
2025-11-27 17:26:04 -0800

    Initial commit - Chaos Caves Forge mod v1.0.0

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chaos Caves Mod
+
+A Forge mod for Minecraft 1.20.1 that creates a cave-only overworld with bedrock ceiling.
+
+## Features
+- Bedrock ceiling at Y 315-320
+- All biomes spawn as cave versions
+- Caves extend from Y -64 to Y 300
+- Compatible with all biome mods (Biomes O' Plenty, etc.)
+- Structures and features spawn at cave floor level
+- Server-side compatible
+
+## Building
+
+Requirements:
+- Java JDK 17
+- Gradle (included via wrapper)
+
+To build:
+```
+gradlew build
+```
+
+The compiled mod will be in `build/libs/chaoscaves-1.0.0.jar`
+
+## Installation
+
+1. Install Minecraft Forge 1.20.1
+2. Place the .jar file in your mods folder
+3. Launch Minecraft
+
+## Configuration
+
+No configuration needed - works out of the box!
---
+++ src/main/java/com/chaoscaves/ChaosCavesMod.java
@@ -0,0 +1,32 @@
+package com.chaoscaves;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod("chaoscaves")
+public class ChaosCavesMod {
+    public static final String MODID = "chaoscaves";
+    
+    private static final DeferredRegister<DensityFunction> DENSITY_FUNCTIONS = 
+        DeferredRegister.create(Registries.DENSITY_FUNCTION, MODID);
+
+    public ChaosCavesMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        DENSITY_FUNCTIONS.register(modEventBus);
+    }
+
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MODID, path);
+    }
+    
+    public static ResourceKey<DensityFunction> densityKey(String path) {
+        return ResourceKey.create(Registries.DENSITY_FUNCTION, id(path));
+    }
+}
